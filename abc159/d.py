0baf9d696058f92b6c9ad6168cadabf70be89d23
Takumi Kumagai <kumagai.takumi1223@gmail.com>
2020-04-05 21:20:22 +0900

    additional past problems

--- abc159/d.py
+++ abc159/d.py
@@ -1,12 +1,19 @@
 import math
 input()
 items = list(map(int,input().split()))
-for i in range(len(items)):    
-    rest = items[:i] + items[i+1:]
-    rest_unique = set(rest)
-    out = 0
-    for j in rest_unique:
-        cnt = sum([1 for x in rest if x == j])
-        if cnt >= 2:
-            out = out + math.factorial(cnt)/math.factorial(cnt-2)/2
-    print(int(out))
+d = {}
+for i in items:
+    if i in d.keys():
+        d[i] = d[i]+1
+    else:
+        d[i] = 1
+
+p = {}
+for k in d.keys():
+    cnt = d[k]
+    p[k] = (cnt*(cnt-1))/2
+
+total = sum(p.values())
+
+for item in items:
+    print(int(total - d[item] + 1))
