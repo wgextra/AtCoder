0ce7433b14fb0e4cc4baa235e8a6faa33267c6b3
Takumi Kumagai <kumagai.takumi1223@gmail.com>
2020-04-26 00:32:58 +0900

    union_findデバグ。

--- lib/union_find.py
+++ lib/union_find.py
@@ -10,23 +10,20 @@ class UnionFind:
             return self.find(self.parent[a])
 
     def unite(self,a,b):
-        a_parent = self.parent[a]
-        b_parent = self.parent[b]
-        if a_parent == b_parent:
+        a_root = self.find(a)
+        b_root = self.find(b)
+        if a_root == b_root:
             return
         else:
-            if self.size_of(a_parent) > self.size_of(b_parent):
-                self.parent[b_parent] = self.parent[a_parent]
-            elif self.size_of(b_parent) > self.size_of(a_parent): 
-                self.parent[a_parent] = self.parent[b_parent]
+            if self.size_of(b_root) > self.size_of(a_root): 
+                self.parent[a_root] = self.parent[b_root]
+                self.size[b_root] += self.size[a_root]
             else:
-                self.parent[b_parent] = self.parent[a_parent]
-                self.size[a_parent] += 1
-                print(self.size)
-                print(self.parent)
+                self.parent[b_root] = self.parent[a_root]
+                self.size[a_root] += self.size[b_root]
                 
     def size_of(self,a):
-        return self.size[self.parent[a]]
+        return self.size[self.find(a)]
     
     def is_same(self,a,b):
-        return self.parent(a) == self.parent(b)
+        return self.find(a) == self.find(b)
